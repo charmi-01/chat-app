181c2855c8cb7e832311850f0a9104ebec16f576
chirag <chiragsharma1676@gmail.com>
2022-06-22 11:42:33 +0530

    created rooms context

---
+++ src/context/rooms.context.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/function-component-definition */
+import React,{ createContext, useState,useEffect}  from 'react';
+import { database } from '../misc/firebase';
+import { transformToArrayWithId } from '../misc/helpers';
+
+
+
+const RoomsContext= createContext();
+
+export const RoomsProvider=({children})=>{
+
+    const [rooms,setRooms]=useState(null);
+
+    useEffect(()=>{
+        const roomListRef= database.ref('rooms');
+
+        roomListRef.on('value',(snap)=>{
+            const data= transformToArrayWithId(snap.val());
+            setRooms(data);
+        });
+
+        return ()=>{
+            roomListRef.off();
+        };
+
+    },[]);
+
+    return <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+}
+
